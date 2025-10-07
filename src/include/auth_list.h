580dbdb5098c07b9575f2d1812428ba030a3a864
AliGhaffarian <alighaffarian9@gmail.com>
2025-10-07 19:38:46 +0330

    auth_list: introduce k1_record and include vmlinux.h conditionally
    Introduce k1_record for bpf programs that don't have direct access to
    uid (kernel mods)
    Include vmlinux.h conditionally to avoid leaking it into userspace

--- src/include/auth_list.h
+++ src/include/auth_list.h
@@ -1,10 +1,28 @@
 #ifndef AUTH_LIST
 #define AUTH_LIST
 
-#include <stdbool.h>
+#ifdef __BPF__
+#include <vmlinux.h>
+#else
+#include <sys/types.h>
+#endif
+
 #include <k1_limits.h>
 #include <auth_check.h>
 #include <verdict.h>
+
+struct k1_record {
+    uid_t uid;
+    bool is_authenticated;
+    enum K1_VERDICT_HOOK verdict_hook;
+    struct k1_auth_check_detail auth_check_detail;
+};
+
+struct k1_record_list {
+    int len;
+    struct k1_record records[K1_MAX_USER_RECORDS];
+};
+
 
 struct k1_sys_record {
     bool is_authenticated;
