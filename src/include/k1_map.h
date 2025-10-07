4c39b74d9e55e9b9026588f07f54ba2232641bc9
AliGhaffarian <alighaffarian9@gmail.com>
2025-10-07 19:40:51 +0330

    k1_map: inroduce k1_auth_map_hash

--- src/include/k1_map.h
+++ src/include/k1_map.h
@@ -5,6 +5,13 @@
 #include <bpf/bpf_helpers.h>
 #include <auth_list.h>
 
+struct k1_auth_map_hash {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 1024);
+    __type(key, __u32);
+    __type(value, struct k1_record_list);
+    __uint(pinning, LIBBPF_PIN_BY_NAME);
+};
 
 struct k1_auth_map_hash_sys {
     __uint(type, BPF_MAP_TYPE_HASH);
