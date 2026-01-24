e73e8c5304e36efb06d7494b39309fb4854418c4
AliGhaffarian <alighaffarian9@gmail.com>
2026-01-24 15:21:45 +0330

    bpf_util: add k1_bpf_lookup_auth_record()
    Finds the record corresponding with the given key, can set uid to
    INVALID_UID to find for any registered uid.

--- src/bpf/include/k1_bpf_util.h
+++ src/bpf/include/k1_bpf_util.h
@@ -9,9 +9,16 @@
 #include <bpf/bpf_helpers.h>
 #endif
 
+#include <auth_record.h>
+#include <errno.h>
+#include <helper.h>
 #include <k1_limits.h>
 #include <k1_map.h>
+#include <k1_map_keys_values.h>
 #include <verdict_record.h>
+
+#define BPF_FOR_EACH_MAP_ELEM_STOP     1
+#define BPF_FOR_EACH_MAP_ELEM_CONTINUE 0
 
 inline int k1_strcmp(char *first, char *second) {
     int cnt = 0;
@@ -67,4 +74,73 @@ inline void k1_change_user_auth_state(
     k1_do_op_on_flag(&verdict_list_elem->is_authenticated, op);
 }
 
+struct find_auth_record_ctx {
+    struct k1_sys_auth_map_key *key;
+    struct k1_auth_record *result;
+};
+
+/**
+ * @return on success, ctx->result points to the found auth_record, with the
+ * related uid in ctx->key, on error, uid of ctx->key is INVALID_UID
+ */
+static long first_auth_record_with_uid_of_context(
+    struct bpf_map *registered_map,
+    const void *current_uid_key,
+    void *value,
+    void *ctx) {
+    void *lookup_result;
+    struct find_auth_record_ctx *ctx_casted = ctx;
+    __u32 current_uid = *(__u32 *)current_uid_key;
+
+    AUTHMAP_KEY_SET_UID(ctx_casted->key, current_uid);
+
+    lookup_result = bpf_map_lookup_elem(&sys_auth_map_hash, ctx_casted->key);
+
+    if(!lookup_result) {
+        AUTHMAP_KEY_SET_UID(ctx_casted->key, INVALID_UID);
+        return BPF_FOR_EACH_MAP_ELEM_CONTINUE;
+    }
+
+    ctx_casted->result = lookup_result;
+    return BPF_FOR_EACH_MAP_ELEM_STOP;
+}
+
+inline void *
+_k1_bpf_lookup_auth_record_any_uid(struct k1_sys_auth_map_key *key) {
+    __u32 tmp_ptr;
+    __u32 uid_ptr;
+    long err;
+
+    struct find_auth_record_ctx ctx = {
+        .key = key,
+        .result = NULL,
+    };
+
+    err = bpf_for_each_map_elem(
+        &registered_uids_map_hash,
+        first_auth_record_with_uid_of_context,
+        &ctx,
+        0);
+    if(err == -EINVAL)
+        return NULL;
+
+    return ctx.result;
+}
+
+/**
+ * @brief lookup the auth_record related to the given key
+ *
+ * @param key key to search, use AUTHMAP_KEY_SET_UID(*key, INVALID_UID) to
+ * search for any uid related to the rest of the key
+ *
+ * @return result of bpf_map_lookup_elem, if uid is INVALID_UID, result of the
+ * last called bpf_map_lookup_elem if returned
+ */
+inline struct k1_sys_record *
+k1_bpf_lookup_auth_record(struct k1_sys_auth_map_key *key) {
+    if(AUTHMAP_KEY_GET_UID(key) == INVALID_UID) {
+        return _k1_bpf_lookup_auth_record_any_uid(key);
+    }
+    return bpf_map_lookup_elem((void *)&sys_auth_map_hash, (void *)key);
+}
 #endif
