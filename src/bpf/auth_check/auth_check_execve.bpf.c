66a43174d8495f33c6b2b0e44eac7435c41cc68b
AliGhaffarian <alighaffarian9@gmail.com>
2026-02-03 23:53:18 +0330

    bpf: fix uid truncation

--- src/bpf/auth_check/auth_check_execve.bpf.c
+++ src/bpf/auth_check/auth_check_execve.bpf.c
@@ -22,7 +22,7 @@ int BPF_PROG(auth_cred_execve_check, void *a, void *b, char *filename) {
     bpf_core_read_user(buf, K1_BPF_STRING_MAXSIZE - 1, filename);
     buf[K1_BPF_STRING_MAXSIZE - 1] = 0;
 
-    u32 uid = bpf_get_current_uid_gid() & 0xffff;
+    u32 uid = bpf_get_current_uid_gid() & 0xffffffff;
     struct k1_auth_map_key key = {
         .uid = uid,
         .auth_type = K1_AUTH_TYPE_EXECVE,
