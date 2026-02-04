8d8a548bc633d72c37a38b0dfebaf3870ba1a50e
AliGhaffarian <alighaffarian9@gmail.com>
2026-02-04 18:48:49 +0330

    common: add maps for `enum K1_VERDICT_MAP_TYPE`, add _*_UNSPEC entries

--- src/common/enum_to_str_maps.c
+++ src/common/enum_to_str_maps.c
@@ -1,6 +1,7 @@
 #include <auth_cred.h>
 #include <helper.h>
 #include <string.h>
+#include <k1_map_keys_values.h>
 
 #include "enum_to_str_maps.h"
 
@@ -12,6 +13,7 @@ enum K1_AUTH_TYPE enum_from_string_k1_auth_type(char *str) {
 };
 
 char *enum_to_string_k1_auth_type[] = {
+    [_K1_AUTH_UNSPEC] = STR(_K1_AUTH_UNSPEC),
     [K1_AUTH_TYPE_EXECVE] = STR(K1_AUTH_TYPE_EXECVE),
     [K1_AUTH_TYPE_USB] = STR(K1_AUTH_TYPE_USB),
 };
@@ -24,6 +26,21 @@ enum K1_VERDICT_HOOK enum_from_string_k1_verdict_hook(char *str) {
 };
 
 char *enum_to_string_k1_verdict_hook[] = {
+    [_K1_VERDICT_HOOK_UNSPEC] = STR(_K1_VERDICT_HOOK_UNSPEC),
     [K1_VERDICT_HOOK_LSM_BPRM_CREDS_FOR_EXEC] =
         STR(K1_VERDICT_HOOK_LSM_BPRM_CREDS_FOR_EXEC),
+};
+
+enum K1_VERDICT_MAP_TYPE enum_from_string_k1_verdict_map_type(char *str) {
+    for(int i = 0; i < _K1_VERDICT_MAP_SIZE; i++)
+        if(!strcmp(str, enum_to_string_k1_verdict_map_type[i]))
+            return i;
+    return _K1_VERDICT_MAP_UNSPEC;
+};
+
+char *enum_to_string_k1_verdict_map_type[] = {
+    [_K1_VERDICT_MAP_UNSPEC] = STR(_K1_VERDICT_MAP_UNSPEC),
+    [K1_VERDICT_MAP_UID] =
+        STR(K1_VERDICT_MAP_UID),
+    [K1_VERDICT_MAP_SID] = STR(K1_VERDICT_MAP_UID)
 };
--- src/common/enum_to_str_maps.h
+++ src/common/enum_to_str_maps.h
@@ -10,4 +10,6 @@ enum K1_AUTH_TYPE enum_from_string_k1_auth_type(char *);
 extern char *enum_to_string_k1_verdict_hook[];
 enum K1_VERDICT_HOOK enum_from_string_k1_verdict_hook(char *);
 
+extern char *enum_to_string_k1_verdict_map_type[];
+enum K1_VERDICT_MAP_TYPE enum_from_string_k1_verdict_map_type(char *);
 #endif
