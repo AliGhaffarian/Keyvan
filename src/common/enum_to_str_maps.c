94dc2c01f5ad8f2f5f8b8844e178a522b017a362
AliGhaffarian <alighaffarian9@gmail.com>
2026-02-12 19:32:17 +0330

    common: add hash_algo_name mapping
    This enum to string mapping is copied from linux source tree

--- src/common/enum_to_str_maps.c
+++ src/common/enum_to_str_maps.c
@@ -1,6 +1,7 @@
 #include <auth_cred.h>
 #include <helper.h>
 #include <k1_map_pairs.h>
+#include <linux/hash_info.h>
 #include <string.h>
 
 #include "enum_to_str_maps.h"
@@ -45,3 +46,39 @@ char *enum_to_string_k1_verdict_map_type[] = {
     [_K1_VERDICT_MAP_UNSPEC] = STR(_K1_VERDICT_MAP_UNSPEC),
     [K1_VERDICT_MAP_UID] = STR(K1_VERDICT_MAP_UID),
     [K1_VERDICT_MAP_SID] = STR(K1_VERDICT_MAP_SID)};
+
+enum hash_algo enum_from_string_hash_algo(char *str)
+{
+    for(int i = 0; i < HASH_ALGO__LAST; i++)
+        if(!strcmp(str, enum_to_string_k1_verdict_map_type[i]))
+            return i;
+    return -1;
+};
+
+// copied from
+// https://elixir.bootlin.com/linux/v6.19-rc5/source/lib/crypto/hash_info.c#L11
+const char *const hash_algo_name[HASH_ALGO__LAST] = {
+    [HASH_ALGO_MD4] = "md4",
+    [HASH_ALGO_MD5] = "md5",
+    [HASH_ALGO_SHA1] = "sha1",
+    [HASH_ALGO_RIPE_MD_160] = "rmd160",
+    [HASH_ALGO_SHA256] = "sha256",
+    [HASH_ALGO_SHA384] = "sha384",
+    [HASH_ALGO_SHA512] = "sha512",
+    [HASH_ALGO_SHA224] = "sha224",
+    [HASH_ALGO_RIPE_MD_128] = "rmd128",
+    [HASH_ALGO_RIPE_MD_256] = "rmd256",
+    [HASH_ALGO_RIPE_MD_320] = "rmd320",
+    [HASH_ALGO_WP_256] = "wp256",
+    [HASH_ALGO_WP_384] = "wp384",
+    [HASH_ALGO_WP_512] = "wp512",
+    [HASH_ALGO_TGR_128] = "tgr128",
+    [HASH_ALGO_TGR_160] = "tgr160",
+    [HASH_ALGO_TGR_192] = "tgr192",
+    [HASH_ALGO_SM3_256] = "sm3",
+    [HASH_ALGO_STREEBOG_256] = "streebog256",
+    [HASH_ALGO_STREEBOG_512] = "streebog512",
+    [HASH_ALGO_SHA3_256] = "sha3-256",
+    [HASH_ALGO_SHA3_384] = "sha3-384",
+    [HASH_ALGO_SHA3_512] = "sha3-512",
+};
--- src/common/enum_to_str_maps.h
+++ src/common/enum_to_str_maps.h
@@ -2,6 +2,7 @@
 #define K1_ENUM_TO_STRING_MAPS
 
 #include <auth_cred.h>
+#include <linux/hash_info.h>
 #include <verdict_record.h>
 
 extern char *enum_to_string_k1_auth_type[];
@@ -12,4 +13,10 @@ enum K1_VERDICT_HOOK enum_from_string_k1_verdict_hook(char *);
 
 extern char *enum_to_string_k1_verdict_map_type[];
 enum K1_VERDICT_MAP_TYPE enum_from_string_k1_verdict_map_type(char *);
+
+enum hash_algo enum_from_string_hash_algo(char *str);
+
+// copied from
+// https://elixir.bootlin.com/linux/v6.19-rc5/source/lib/crypto/hash_info.c#L11
+extern const char *const hash_algo_name[HASH_ALGO__LAST];
 #endif
