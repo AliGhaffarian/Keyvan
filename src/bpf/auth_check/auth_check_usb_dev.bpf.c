9866a72956d8f1a6cd994537d29b6b341e51e187
AliGhaffarian <alighaffarian9@gmail.com>
2026-04-18 00:46:33 +0330

    auth_check_usb_dev: fix credentials being ignored
    Ignoring credentials started since commit: 6d731fae3e1ab552cef37b746e92feb0e6d1ac89,
    when the lookup of records were unified. I removed the credential
    compare line and forgot to put it back.

--- src/bpf/auth_check/auth_check_usb_dev.bpf.c
+++ src/bpf/auth_check/auth_check_usb_dev.bpf.c
@@ -17,24 +17,32 @@ static const enum K1_AUTH_TYPE const_auth_type_usb = K1_AUTH_TYPE_USB;
 SEC("fentry/usb_create_sysfs_dev_files")
 int BPF_PROG(auth_check_usb_create_sysfs, struct usb_device *udev)
 {
-    if(!udev->serial)
-        return 0;
-
-    bpf_printk("connected udev: %s", udev->serial);
+    int do_auth = 0;
 
     struct k1_auth_map_key key = {
         .euid = INVALID_UID,
         .auth_type = K1_AUTH_TYPE_USB,
     };
     struct k1_auth_map_value *elem = k1_bpf_auth_map_lookup(&key);
-
     if(!elem)
         return 0;
 
-    k1_change_user_auth_state(
-        &elem->verdict_entry_lookup_info, key.euid, K1_FLAG_CHANGE_SET);
+    if(elem->is_authenticated)
+        do_auth = 1;
+    else if(
+        udev->serial &&
+        k1_strcmp(elem->record.auth_cred_usb.serial, udev->serial) == 0)
+        do_auth = 1;
 
-    elem->is_authenticated = 1;
+    if(do_auth) {
+        k1_change_user_auth_state(
+            &elem->verdict_entry_lookup_info, key.euid, K1_FLAG_CHANGE_SET);
+
+        elem->is_authenticated = 1;
+    }
+
+    if(udev->serial)
+        bpf_printk("connected udev: %s", udev->serial);
 
     return 0;
 }
@@ -42,25 +50,30 @@ int BPF_PROG(auth_check_usb_create_sysfs, struct usb_device *udev)
 SEC("fentry/usb_remove_sysfs_dev_files")
 int BPF_PROG(auth_check_usb_remove_sysfs, struct usb_device *udev)
 {
-
-    if(!udev->serial)
-        return 0;
-
-    bpf_printk("disconnected udev: %s", udev->serial);
+    bool do_deauth = 0;
 
     struct k1_auth_map_key key = {
         .euid = INVALID_UID,
         .auth_type = K1_AUTH_TYPE_USB,
     };
     struct k1_auth_map_value *elem = k1_bpf_auth_map_lookup(&key);
-
     if(!elem)
         return 0;
 
-    k1_change_user_auth_state(
-        &elem->verdict_entry_lookup_info, key.euid, K1_FLAG_CHANGE_SET);
+    else if(
+        udev->serial &&
+        k1_strcmp(elem->record.auth_cred_usb.serial, udev->serial) == 0)
+        do_deauth = 1;
 
-    elem->is_authenticated = 0;
+    if(do_deauth) {
+        k1_change_user_auth_state(
+            &elem->verdict_entry_lookup_info, key.euid, K1_FLAG_CHANGE_CLEAR);
+
+        elem->is_authenticated = 0;
+    }
+
+    if(udev->serial)
+        bpf_printk("disconnected udev: %s", udev->serial);
 
     return 0;
 }
